d0642e51710078bf281cabee589559929688cff0
Brice187 <lars@kerick.net>
2016-06-04 22:32:08 +0200

    Added Twitter share Button. TODO: Pre-Fill Tweetsheet with actual Quote

--- scripts/quotemachine.js
+++ scripts/quotemachine.js
@@ -1,12 +1,28 @@
 $.ajaxSetup({
-  cache: false
+    cache: false
 });
 
 $(document).ready(function() {
-  $("#getMessage").click(function() {
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-      $("#message").html(a[0].content + "<p>&mdash; " + a[0].title + "</p>");
+    $("#getMessage").click(function() {
+        $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+            $("#message").html(a[0].content + "<p>&mdash; " + a[0].title + "</p>");
+        });
     });
-    setTimeout("this", 1000);
-  });
 });
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
